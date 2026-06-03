a540b502455918332bf9a2e4f785b39d024e58e0
abhinav6336 <abhinav115ereve@gmail.com>
2026-06-03 19:57:04 +0530

    PRINTING TABLES AND COLUMNS

---
+++ simple.sql
@@ -0,0 +1,15 @@
+-- #STARTING THE SQLITE ENVIRONMENT
+
+-- -> sqlite3 practice.db 
+
+-- #RUNNING THE .sql file
+
+-- -> sqlite3 practice.db < simple.sql
+
+-- #SELECTING ALL TABLES FROM DATABASE
+
+SELECT * FROM "orders";
+
+-- #SELECTRING SPECIFIX TABLE FROM DATABASE
+
+SELECT "product" FROM "orders";
